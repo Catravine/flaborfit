5fbd8f591b155af9f3779e30e9df8927138e2de5
Caroline Courtney <caroline@carolinecourtney.com>
2017-04-23 14:30:06 -0400

    Connecting the two activities

--- app/src/main/java/com/carolinecourtney/flaborfit/DetailsActivity.java
+++ app/src/main/java/com/carolinecourtney/flaborfit/DetailsActivity.java
@@ -2,6 +2,8 @@ package com.carolinecourtney.flaborfit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.ImageView;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        TextView exerciseTitle = (TextView)findViewById(R.id.exerciseTitle);
+        ImageView exersizeImage = (ImageView)findViewById(R.id.exerciseImage);
     }
 }
--- app/src/main/java/com/carolinecourtney/flaborfit/MainActivity.java
+++ app/src/main/java/com/carolinecourtney/flaborfit/MainActivity.java
@@ -1,11 +1,14 @@
 package com.carolinecourtney.flaborfit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_ITEM_TITLE = "extra.item.title";
 
     public static final String EXERCISE_WEIGHT = "Weight Lifting";
     public static final String EXERCISE_YOGA = "Yoga";
@@ -43,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadDetailActivity(String exerciseTitle) {
-
+        Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+        intent.putExtra(MainActivity.EXTRA_ITEM_TITLE, exerciseTitle);
+        startActivity(intent);
     }
 }
