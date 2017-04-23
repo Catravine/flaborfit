b8e7e3f3a228a7dee1f64874202924a31886b150
Caroline Courtney <caroline@carolinecourtney.com>
2017-04-23 18:35:53 -0400

    Settings in a menu, 'night mode' checkbox UI

--- app/src/main/java/com/carolinecourtney/flaborfit/MainActivity.java
+++ app/src/main/java/com/carolinecourtney/flaborfit/MainActivity.java
@@ -3,6 +3,9 @@ package com.carolinecourtney.flaborfit;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
 
@@ -43,6 +46,24 @@ public class MainActivity extends AppCompatActivity {
                 loadDetailActivity(MainActivity.EXERCISE_CARDIO);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+                System.out.println("I picked 'settings'.");
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void loadDetailActivity(String exerciseTitle) {
