e174d3fa903f6d6fec5be70351ef33c67aa10682
Caroline Courtney <caroline@carolinecourtney.com>
2017-04-23 20:25:29 -0400

    Re-added different settings for different exercise details (lost somehow)

--- app/src/main/java/com/carolinecourtney/flaborfit/DetailsActivity.java
+++ app/src/main/java/com/carolinecourtney/flaborfit/DetailsActivity.java
@@ -1,7 +1,9 @@
 package com.carolinecourtney.flaborfit;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.ImageView;
 
@@ -11,8 +13,25 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+//        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+//        System.out.println(sharedPref);
 
-        TextView exerciseTitle = (TextView)findViewById(R.id.exerciseTitle);
+        TextView exerciseText = (TextView)findViewById(R.id.exerciseTitle);
         ImageView exersizeImage = (ImageView)findViewById(R.id.exerciseImage);
+        LinearLayout mainBG = (LinearLayout)findViewById(R.id.mainBG);
+
+        String exerciseTitle = getIntent().getStringExtra(MainActivity.EXTRA_ITEM_TITLE);
+        exerciseText.setText(exerciseTitle);
+
+        if (exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_WEIGHT)) {
+            exersizeImage.setImageDrawable(getResources().getDrawable(R.drawable.weight));
+            mainBG.setBackgroundColor(Color.parseColor("#2ca5f5"));
+        } else if (exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_YOGA)) {
+            exersizeImage.setImageDrawable(getResources().getDrawable(R.drawable.lotus));
+            mainBG.setBackgroundColor(Color.parseColor("#916bcd"));
+        } else {
+            exersizeImage.setImageDrawable(getResources().getDrawable(R.drawable.heart));
+            mainBG.setBackgroundColor(Color.parseColor("#52ad56"));
+        }
     }
 }
--- app/src/main/java/com/carolinecourtney/flaborfit/SettingsActivity.java
+++ app/src/main/java/com/carolinecourtney/flaborfit/SettingsActivity.java
@@ -1,34 +1,36 @@
 package com.carolinecourtney.flaborfit;
 
 import android.support.v7.app.AppCompatActivity;
+import android.preference.PreferenceActivity;
 import android.os.Bundle;
 import android.widget.CompoundButton;
 import android.widget.CheckBox;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-
-        CheckBox nightModeBox = (CheckBox) findViewById(R.id.nightModeBox);
-
-        nightModeBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                //store isChecked to Preferences
-                System.out.println("checked: " + isChecked + "!");
-//                SharedPreferences settings = getSharedPreferences(PREFS,0);
-//                System.out.println(settings);
-//                SharedPreferences.Editor editor = settings.edit();
-//                editor.putBoolean("isChecked", false);
-//                System.out.println(settings);
-
-                //PreferenceManager.getDefaultSharedPreferences(PopUpInfoActivity.this).edit().putBoolean("value", isChecked).apply();
-            }
-        });
+        addPreferencesFromResource(R.xml.preferences);
+//        setContentView(R.layout.activity_settings);
+//
+//        CheckBox nightModeBox = (CheckBox) findViewById(R.id.nightModeBox);
+//
+//        nightModeBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                //store isChecked to Preferences
+//                System.out.println("checked: " + isChecked + "!");
+////                SharedPreferences settings = getSharedPreferences(PREFS,0);
+////                System.out.println(settings);
+////                SharedPreferences.Editor editor = settings.edit();
+////                editor.putBoolean("isChecked", false);
+////                System.out.println(settings);
+//
+//                //PreferenceManager.getDefaultSharedPreferences(PopUpInfoActivity.this).edit().putBoolean("value", isChecked).apply();
+//            }
+//        });
 
     }
 }
